076fc0904a8de3bc4a82639f4a6f01e71be9c8e8
khanaccount1 <khanaccount1@mail.ru>
2025-03-11 03:13:09 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/tzGptBot/",
   plugins: [react()],
   resolve: {
     alias: {
